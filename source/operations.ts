6d352371cfec3d62c0d0714443cbae5680ae6ff2
Devang Patel <devang@spiretrading.com>
2021-08-04 11:43:59 -0400

    Update operation classes

--- source/operations.ts
+++ source/operations.ts
@@ -9,12 +9,16 @@ export class AddRowOperation {
    * Constructs an AddRowOperation.
    * @param index - The index the row was added to.
    */
-  constructor(index: number) {}
+  constructor(index: number) {
+    this._index = index;
+  }
 
   /** Returns the index of the added row. */
   public get index(): number {
-    return 0;
+    return this._index;
   }
+
+  private _index: number;
 }
 
 /** Indicates a row was moved. */
@@ -25,17 +29,23 @@ export class MoveRowOperation {
    * @param source - The index that the row was moved from.
    * @param destination - The index that the row was moved to.
    */
-  constructor(source: number, destination: number) {}
+  constructor(source: number, destination: number) {
+    this._source = source;
+    this._destination = destination;
+  }
 
   /** Returns the index that the row was moved from. */
   public get source(): number {
-    return 0;
+    return this._source;
   }
 
   /** Returns the index that the row was moved to. */
   public get destination(): number {
-    return 0;
+    return this._destination;
   }
+
+  private _source: number;
+  private _destination: number;
 }
 
 /** Indicates a row was removed. */
@@ -45,12 +55,16 @@ export class RemoveRowOperation {
    * Constructs a RemoveRowOperation.
    * @param index - The index of the row that was removed.
    */
-  constructor(index: number) {}
+  constructor(index: number) {
+    this._index = index;
+  }
 
   /** Returns the index where the row was removed from. */
   public get index(): number {
-    return 0;
+    return this._index;
   }
+
+  private _index: number;
 }
 
 /** Indicates a value was updated. */
@@ -61,17 +75,23 @@ export class UpdateOperation {
    * @param row - The index of the row that was updated.
    * @param column - The index of the column that was updated.
    */
-  constructor(row: number, column: number) {}
+  constructor(row: number, column: number) {
+    this._row = row;
+    this._column = column;
+  }
 
   /** Returns the index of the updated row. */
   public get row(): number {
-    return 0;
+    return this._row;
   }
 
   /** Returns the index of the updated column. */
   public get column(): number {
-    return 0;
+    return this._column;
   }
+
+  private _row: number;
+  private _column: number;
 }
 
 /** Indicates multiple operations were executed transactionally. */
@@ -81,10 +101,14 @@ export class Transaction {
    * Constructs a Transaction.
    * @param operations The list of operations executed.
    */
-  constructor(operations: Operation[]) {}
+  constructor(operations: Operation[]) {
+    this._operations = operations;
+  }
 
   /** Returns the list of operations executed. */
   public get operations(): Operation[] {
-    return [];
+    return this._operations;
   }
+
+  private _operations: Operation[];
 }
