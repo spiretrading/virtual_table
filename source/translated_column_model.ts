3e4735fe316f9dec9a323ec94032ec52460aaa76
Sofiya Polyanska <sofiya@spiretrading.com>
2022-05-09 15:53:41 -0400

    Add moveColumn to TranslatedColumnModel

--- source/translated_column_model.ts
+++ source/translated_column_model.ts
@@ -4,13 +4,14 @@ import {AddRowOperation, MoveRowOperation, Operation, RemoveRowOperation,
 import {TableModel} from './table_model';
 import {TransactionLog} from './transaction_log';
 
+/** Adapts an existing TableModel with the ability to move columns. */
 export class TranslatedColumnModel extends TableModel {
   constructor(model: TableModel) {
     super();
     this.sourceTable = model;
-    this.translatedToSourceColumns = [];
+    this.columnOrder = [];
     for(let index = 0; index < model.columnCount; ++index) {
-      this.translatedToSourceColumns.push(index);
+      this.columnOrder.push(index);
     }
     this.transactionLog = new TransactionLog();
   }
@@ -20,15 +21,32 @@ export class TranslatedColumnModel extends TableModel {
   }
 
   public get columnCount(): number {
-    return this.sourceTable.columnCount;
+    return this.columnOrder.length;
   }
 
   public get(row: number, column: number) {
-    if(row >= this.rowCount || row < 0) {
+    if(row >= this.rowCount || row < 0 || column < 0 ||
+        column >= this.columnCount) {
       throw new RangeError(
         'Row or column are outside of the table\'s bounds.');
-    } 
-    return this.sourceTable.get(row, this.translatedToSourceColumns[column]);
+    }
+    return this.sourceTable.get(row, this.columnOrder[column]);
+  }
+
+  /** Moves a column.
+   * @param source - The original position of the column.
+   * @param dest - The new position of the column.
+   * @throws RangeError - The indexes specified is not within range.
+   */
+  public moveColumn(source: number, dest: number): void{
+    if(source < 0 || dest < 0 || source >= this.columnCount ||
+        dest >= this.columnCount) {
+      throw new RangeError(
+        'Source or destination are outside of the table\'s bounds.');
+    }
+    const sourceValue = this.columnOrder[source];
+    this.columnOrder.splice(source, 1);
+    this.columnOrder.splice(dest, 1, sourceValue);
   }
 
   public connect(
@@ -36,7 +54,7 @@ export class TranslatedColumnModel extends TableModel {
     return this.transactionLog.connect(slot);
   }
 
-  translatedToSourceColumns: number[];
+  columnOrder: number[];
   private sourceTable: TableModel;
   private transactionLog: TransactionLog;
 }
