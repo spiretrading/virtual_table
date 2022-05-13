9da4aaa0831e89a2c4ac2765e20b092c03ba1b2e
Sofiya Polyanska <sofiya@spiretrading.com>
2022-05-13 19:10:13 -0400

    Delete translated column model

--- source/index.ts
+++ source/index.ts
@@ -4,5 +4,4 @@ export * from './operations';
 export * from './table_model';
 export * from './table_view';
 export * from './transaction_log';
-export * from './translated_column_model';
 export * from './translated_table_model';
