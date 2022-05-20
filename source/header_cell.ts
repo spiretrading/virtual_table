6d50abf927b027050d263d56462e716a2d8800ca
Sofiya Polyanska <sofiya@spiretrading.com>
2022-05-19 20:25:27 -0400

    Add missing documentation

--- source/header_cell.ts
+++ source/header_cell.ts
@@ -1,7 +1,15 @@
 import {Sorting} from './sorting';
 import {Filter} from './filter';
 
+/** Header of a column of a table. */
 export class HeaderCell {
+
+  /** Constructs a new HeaderCell.
+    * @param name - The name to display if there is enough available space.
+    * @param shortName - The name to display if there is not enough space.
+    * @param isSortable - Indicates if the column is sortable.
+    * @param sorting - The current sorting of the column.
+    */
   constructor(name: string, shortName: string, isSortable: boolean = true,
       sorting: Sorting = Sorting.NONE) {
     this.#name = name;
@@ -14,18 +22,22 @@ export class HeaderCell {
     this.#filterable = new HeaderCell.Unfilterable();
   }
 
+  /** Get the full name of the header. */
   get name(): string {
     return this.#name;
   }
 
+  /** Get the condesned name of the header. */
   get shortName(): string {
     return this.#shortName;
   }
 
+  /** Get sortable object from the header. */
   get sortable(): HeaderCell.Sortable | HeaderCell.Unsortable {
     return this.#sorting;
   }
 
+  /** Get filterable object from the header. */
   get filterable(): HeaderCell.Unfilterable {
     return this.#filterable;
   }
@@ -40,14 +52,22 @@ export namespace HeaderCell {
 
   /** Sortable header. */
   export class Sortable {
+
+    /** Constructs a new Sortable.
+     * @param sorting - The initial sorting of the column.
+     */
     constructor(sorting: Sorting) {
       this.#sorting = sorting;
     }
 
+    /** Current sort order of the header. */
     get sortOrder(): Sorting {
       return this.#sorting;
     }
 
+    /** Updates the sorting of the column.
+     * @param sortOrder - The new sorting. 
+     */
     updateSorting(sortOrder: Sorting): void {
       this.#sorting = sortOrder;
     }
@@ -55,13 +75,19 @@ export namespace HeaderCell {
     #sorting: Sorting;
   }
 
+  /** A header that cannot be sorted. */
   export class Unsortable {
+
+    /** Current sort order of the header. */
     get sortOrder(): Sorting {
       return Sorting.NONE
     }
   }
 
+  /** A header that cannot be filtered. */
   export class Unfilterable {
+    
+    /** Get the filter of the header. */
     get filter(): Filter {
       return Filter.NONE;
     }
