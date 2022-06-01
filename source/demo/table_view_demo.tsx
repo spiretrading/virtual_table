5ac897d24762cbbc678657544008755e6a607f2e
Sofiya Polyanska <sofiya@spiretrading.com>
2022-05-31 20:16:33 -0400

    Fix onSort

--- source/demo/table_view_demo.tsx
+++ source/demo/table_view_demo.tsx
@@ -64,7 +64,7 @@ export class TableViewDemo extends React.Component<{}, State> {
   }
 
   private onSort = (column: number, sortOrder: SortOrder) => {
-    this.sortedModel.updateSort(column, sortOrder);
+    this.sortedModel.updateSortOrder(column, sortOrder);
     this.setState({
       headerCells: this.state.headerCells,
       highestPriorityHeader: column
